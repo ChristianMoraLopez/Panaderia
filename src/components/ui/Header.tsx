e91f46dca690eeddfc7c18d7c3ef37a0bb925835
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-10-28 18:06:41 -0500

    iconos

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import styles from '@/styles/Header.module.css';
-
 
 interface HeaderProps {
   title: string;
-  backgroundColor: string;
-  textColor: string;
+  backgroundColor: string; // Clase de Tailwind para el color de fondo
+  textColor: string; // Clase de Tailwind para el color del texto
 }
 
 const Header: React.FC<HeaderProps> = ({ title, backgroundColor, textColor }) => {
   return (
-    <div className="relative mb-12 overflow-hidden min-h-[200px]">
+    <div className="relative mb-12 overflow-hidden">
       {/* Capa de fondo con color */}
       <div className={`absolute inset-0 ${backgroundColor}`} />
       
       {/* Capa de SVG */}
       <div 
-        className={`absolute inset-0 ${styles.backgroundSvg}`}
+        className="absolute inset-0 opacity-50 bg-cover sm:bg-contain" // Aplicamos bg-cover en móviles y bg-contain en sm o mayores
         style={{
           backgroundImage: `url('/images/iconosheader.svg')`,
-          opacity: 0.5,
+          backgroundRepeat: 'repeat-x',
+          backgroundPosition: 'center',
         }}
       />
       
       {/* Contenido */}
-      <div className="relative z-10 py-8 px-4 min-h-[200px] flex items-center justify-center">
-        <h2 className={`text-4xl md:text-5xl lg:text-6xl font-bold text-center ${textColor} title-font`}>
+      <div className="relative z-10 py-8 px-4">
+        <h2
+          className={`text-4xl font-bold text-center ${textColor}`}
+        >
           {title}
         </h2>
       </div>
